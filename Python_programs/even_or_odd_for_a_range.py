20b9d0b02c4b7d4b4cf09147b829f898f6570421
Yuktha Yashaswi <yukthayashaswi2000@gmail.com>
2020-09-06 06:49:14 +0530

    Classify if in a range the number is even or odd

---
+++ Python_programs/even_or_odd_for_a_range.py
@@ -0,0 +1,7 @@
+m = int(input("Enter a number : "))
+n = int(input("Enter a number : "))
+for i in range(m,n+1):
+    if(i%2==0):
+        print(i,"is an even number")
+    else:
+        print(i,"is a odd number")
